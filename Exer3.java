48c7407de606d527986c16b3ac3b31b50632f5b1
gabriel.rost <110986987+gabriel-rost@users.noreply.github.com>
2024-02-19 22:17:54 -0300

    Add files via upload

---
+++ Exer3.java
@@ -0,0 +1,15 @@
+
+public class Exer3 {
+	
+	public static void main (String[] args) {
+		
+		int val1, val2, val3;
+		val1 = 7;
+		val2 = 8;
+		val3 = 5;
+		
+		float media = ((float)(val1+val2+val3)/3);
+		System.out.printf("media: %.2f", media);
+	}
+}
+
